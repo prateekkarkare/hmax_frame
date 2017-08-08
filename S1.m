d7c72ca5c7b149c05ee44f098750cc9fc7f4b449
Prateek <pkarkare@ntu.edu.sg>
2017-08-08 16:04:44 +0800

    divided filter response by l^2 norm of its support; addded exampleimages.mat

--- S1.m
+++ S1.m
@@ -3,10 +3,17 @@ function [ s1ResponseMap ] = S1( filterMatrixCell, image )
 %   Detailed explanation goes here
     s1ResponseMap = cell(size(filterMatrixCell));
     
+    imageSquared = image.^2;
+    l2Norm = cell(size(filterMatrixCell, 1), 1);
+    for scale = 1:size(filterMatrixCell, 1)
+        l2Norm{scale} = conv2(imageSquared, ones(size(filterMatrixCell{scale})), 'same').^0.5;
+        l2Norm{scale} = l2Norm{scale} + ~l2Norm{scale};     %Making 0 elements in norm to 1 to avoid div by 0
+    end
+    
     for scale = 1:size(filterMatrixCell, 1)
         for orientation = 1:size(filterMatrixCell, 2)
             filter = filterMatrixCell{scale,orientation};
-            s1ResponseMap{scale, orientation} = abs(conv2(image,filter, 'same'));
+            s1ResponseMap{scale, orientation} = abs(conv2(image, filter, 'same')) ./ l2Norm{scale};
         end
     end
 end
